f3da1396e3e0338b156dd07192fe419ba54e4801
aykutaltay <aykutaltay1122@gmail.com>
2021-04-17 19:48:41 +0300

    logs ekleme

---
+++ Yazilim.Entities/Log/Logs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DC = Dapper.Contrib.Extensions;
+
+namespace Yazilim.Entities.Log
+{
+    [DC.Table("Logs")]
+    public class Logs
+    {
+        [Browsable(false)]
+        [DC.Key]
+        public int LogId { get; set; }
+        [DisplayName("Log Zaman")]
+        public DateTime LogDate { get; set; }
+        [DisplayName("Konu")]
+        public string Thread { get; set; }
+        [DisplayName("Seviyesi")]
+        public string LogLevel { get; set; }
+        [DisplayName("LogTipi")]
+        public string LogType { get; set; }
+        [DisplayName("LogMesaj")]
+        public string LogMessage { get; set; }
+     
+        [DisplayName("LogListesi")]
+        public string LogObject { get; set; }
+        [DisplayName("Tablo")]
+        public string TableName { get; set; }
+    }
+}
